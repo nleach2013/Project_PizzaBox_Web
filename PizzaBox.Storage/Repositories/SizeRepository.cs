06d4445767733b4a036b3c09bca0d40f7c7847c8
Noah Leach <nleach2013@fau.edu>
2021-05-05 17:45:43 -0400

    Repositories

---
+++ PizzaBox.Storage/Repositories/SizeRepository.cs
@@ -0,0 +1,28 @@
+using PizzaBox.Domain.Interfaces;
+using PizzaBox.Domain.Models;
+
+namespace PizzaBox.Storage.Repositories
+{
+  public class SizeRepository : IRepository<Size>
+  {
+    public bool Delete()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public bool Insert()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public System.Collections.Generic.IEnumerable<Size> Select()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public Size Update()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
---
+++ PizzaBox.Storage/Repositories/ToppingRepository.cs
@@ -0,0 +1,28 @@
+using PizzaBox.Domain.Interfaces;
+using PizzaBox.Domain.Models;
+
+namespace PizzaBox.Storage.Repositories
+{
+  public class ToppingRepository : IRepository<Topping>
+  {
+    public bool Delete()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public bool Insert()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public System.Collections.Generic.IEnumerable<Topping> Select()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public Topping Update()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
