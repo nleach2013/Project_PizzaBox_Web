66616bb90adb6453cafff4e3984e6d162bc42232
Noah Leach <nleach2013@fau.edu>
2021-05-11 23:48:52 -0400

    Revert "Updated controllers"
    This reverts commit 0357452f9b581707e88eee6b34671ae8e179a3dc.

--- PizzaBox.Client/Startup.cs
+++ PizzaBox.Client/Startup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,49 +15,53 @@ namespace PizzaBox.Client
 {
   public class Startup
   {
-    public IConfiguration Configuration { get; set; }
+
+    public IConfiguration Configuration { get; }
 
     public Startup(IConfiguration configuration)
     {
       Configuration = configuration;
     }
 
+    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllersWithViews();
       services.AddScoped<UnitOfWork>();
       services.AddDbContext<PizzaBoxContext>(options =>
       {
-        if (!string.IsNullOrWhiteSpace(Configuration.GetConnectionString("mssql")))
+        options.UseSqlServer(Configuration.GetConnectionString("mssql"), opts =>
         {
-          options.UseSqlServer(Configuration.GetConnectionString("mssql"), opts =>
-          {
-            opts.EnableRetryOnFailure(3);
-          });
-        }
-        else
-        {
-          options.UseNpgsql(Configuration.GetConnectionString("pgsql"), opts =>
-          {
-            opts.EnableRetryOnFailure(3);
-          });
-        }
+          opts.EnableRetryOnFailure(3);
+        });
       });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, PizzaBoxContext context)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
 
       app.UseRouting();
 
+      app.UseAuthorization();
+
       app.UseEndpoints(endpoints =>
       {
-        endpoints.MapControllers();
+        endpoints.MapControllerRoute(
+                  name: "default",
+                  pattern: "{controller=Home}/{action=Index}/{id?}");
       });
     }
   }
